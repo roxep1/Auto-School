f71bd5fecb460b1cfa67f881b8b80055de26c8c6
Philipp <isip_f.v.bashkir@mpt.ru>
2021-10-16 13:16:19 +0300

    structuring

--- app/src/main/java/com/bashkir/auto_school/student/MainStudentActivity.kt
+++ app/src/main/java/com/bashkir/auto_school/student/MainStudentActivity.kt
@@ -1,11 +1,12 @@
-package com.bashkir.auto_school
+package com.bashkir.auto_school.student
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bashkir.auto_school.R
 
-class MainActivity : AppCompatActivity() {
+class MainStudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_student)
     }
 }
